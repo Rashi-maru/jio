c02d6afb703d5180d8093201b088dd1a1d262320
Rashi-maru <70251316+Rashi-maru@users.noreply.github.com>
2021-09-14 16:35:19 +0530

    final updated

--- server/model/schema.js
+++ server/model/schema.js
@@ -16,4 +16,78 @@ const uploadSchema = new mongoose.Schema({
     }
 })
 
+
+// const detailsSchema = new mongoose.Schema({
+//     personName: {
+//         type: String,
+//         required: true
+//     },
+//     contentKind: {
+//         type: String,
+//         description: {
+//             type: String,
+//             possibleValues: ['Article', 'Featured Project Report', 'Project Brief', 'Data Drishti', 'Product(Self Service Tool)']
+//         },
+//         required: true
+//     },
+//     heading: {
+//         type: String,
+//         required: true
+//     },
+//     authors: {
+//         type: String,
+//         required: true
+//     },
+//     projectDescription: {
+//         type: String,
+//         required: true
+//     },
+//     businessValue: {
+//         type: String,
+//         possibleValues: ['Contribution to Revenue', 'Increased Effectiveness or Productivity ', 'Improved Resilience or Reliability ', 'Cost Reduction or Avoidance ', 'Improved uality / Safety / Compliance ', 'Risk Mitigation ', 'User Satisfaction ', 'Not Applicable '],
+//         required: true
+//     },
+//     businessFunction: {
+//         type: String,
+//         businessFunctions: ['Marketing', 'Operations ', 'Finance ', 'HR ', 'Security ', 'Others '],
+//         required: true
+//     },
+//     businessTag: {
+//         type: String,
+//         businessTags: ['R&M', 'JioBP ', 'PetChem ', 'MFG ', 'P&C ', 'FC&A ', 'SCM ', 'RPMG ', 'R&D ', 'IT ', 'Corporate Security ', 'Corporate Services ', 'E&P ', 'Retail ', 'Others '],
+//         required: true
+//     },
+//     technology: {
+//         type: String,
+//         tagTechnology: ['Image & Video Analytics ', 'Video & Video Analytics ', 'Intelligent Automation ', 'Optimization & Simulation ', 'Visualization ', 'Machine Learning / Deep Learning & Statistics ', 'Others '],
+//         required: true
+//     },
+//     otherTags: {
+//         type: String,
+//         required: false
+//     },
+//     result: {
+//         type: String,
+//         required: true
+//     },
+//     businessContext: {
+//         type: String,
+//         required: true
+//     },
+//     methodology: {
+//         type: String,
+//         required: true
+//     },
+//     usage: {
+//         type: String,
+//         required: true
+//     },
+//     bsValue: {
+//         type: String,
+//         required: true
+//     }
+// })
+
+
 module.exports = UploadModel = mongoose.model('uploads', uploadSchema)
+    // module.exports = UploadDetailsModel = mongoose.model('details', detailsSchema)
